7be968759c7775d218c8a171eb40a316a66b1d9b
AleksandraBielecka <aleksandrab55@gmail.com>
2018-11-13 19:43:50 +0100

    Loading employee from data base

--- Hospital_Searcher/MainWindow.xaml.cs
+++ Hospital_Searcher/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Hospital_Searcher
 {
@@ -23,6 +25,50 @@ namespace Hospital_Searcher
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn_Search_Click(object sender, RoutedEventArgs e)
+        {
+            bool isLoaded = false;
+            string CS = @"data source=DESKTOP-E9EAAOK\SQLEXPRESS01;database=HospitalsEmployees;integrated security=SSPI";
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select top 1 FirstName, LastName, Address, Telephone, Email, Salary from tblEmployee where LastName='" + tb_Search.Text+"'", con);
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        var firstName = rdr["FirstName"] as string;
+                        var lastName = rdr["LastName"] as string;
+                        var address = rdr["Address"] as string;
+                        var telephone = rdr["Telephone"] as string;
+                        var email = rdr["Email"] as string;
+                        var salary = Convert.ToInt32(rdr["Salary"]);
+                        tb_FirstName.Text = firstName;
+                        tb_LastName.Text = lastName;
+                        tb_Address.Text = address;
+                        tb_Telephone.Text = telephone;
+                        tb_Email.Text = email;
+                        tb_Salary.Text = salary.ToString();
+                        isLoaded = true;
+                    }
+
+                }
+            }
+            if (isLoaded)
+            {
+                lbl_Status.Content = "Pobrano z bazy danych pracownika.";
+                lbl_Status.Foreground = Brushes.Green;
+                btn_Save.Content = "Zaktualizuj";
+                btn_Save.IsEnabled = true;
+            }
+            else
+            {
+                lbl_Status.Content = "Nie znaleziono pracownika o nazwisku " + tb_Search.Text;
+                lbl_Status.Foreground = Brushes.Red;
+                btn_Save.IsEnabled = false;
+            }
         }
     }
 }
