d42141f7f00b046a110d189b47dcd80a7118f347
AleksandraBielecka <aleksandrab55@gmail.com>
2018-11-13 20:16:48 +0100

    Make user interface more friendly.

--- Hospital_Searcher/MainWindow.xaml.cs
+++ Hospital_Searcher/MainWindow.xaml.cs
@@ -22,19 +22,43 @@ namespace Hospital_Searcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int _currentEmployeeId;
+
         public MainWindow()
         {
             InitializeComponent();
+            ClearTextboxes();
+            btn_Search.Content = "Szukaj";
+            tb_Search.Text = "";
+            lbl_Address.Content = "Address";
+            lbl_email.Content = "Email";
+            lbl_FirstName.Content = "FirstName";
+            lbl_LastName.Content = "LastName";
+            lbl_Salary.Content = "Salary";
+            lbl_Status.Content = "Użyj wyszukiwarki, żeby znaleźć pracownika";
+            lbl_Telephone.Content = "Content";
+            btn_Save.Visibility = Visibility.Hidden;
+        }
+
+        private void ClearTextboxes()
+        {
+            tb_FirstName.Text = "";
+            tb_LastName.Text = "";
+            tb_Address.Text = "";
+            tb_Telephone.Text = "";
+            tb_Email.Text = "";
+            tb_Salary.Text = "";
         }
 
         private void btn_Search_Click(object sender, RoutedEventArgs e)
         {
             bool isLoaded = false;
+            _currentEmployeeId = 0;
             string CS = @"data source=DESKTOP-E9EAAOK\SQLEXPRESS01;database=HospitalsEmployees;integrated security=SSPI";
             using (SqlConnection con = new SqlConnection(CS))
             {
                 con.Open();
-                SqlCommand cmd = new SqlCommand("Select top 1 FirstName, LastName, Address, Telephone, Email, Salary from tblEmployee where LastName='" + tb_Search.Text+"'", con);
+                SqlCommand cmd = new SqlCommand("Select top 1 ID, FirstName, LastName, Address, Telephone, Email, Salary from tblEmployee where LastName='" + tb_Search.Text + "'", con);
                 using (SqlDataReader rdr = cmd.ExecuteReader())
                 {
                     while (rdr.Read())
@@ -51,6 +75,7 @@ namespace Hospital_Searcher
                         tb_Telephone.Text = telephone;
                         tb_Email.Text = email;
                         tb_Salary.Text = salary.ToString();
+                        _currentEmployeeId = Convert.ToInt32(rdr["ID"]);
                         isLoaded = true;
                     }
 
@@ -62,12 +87,15 @@ namespace Hospital_Searcher
                 lbl_Status.Foreground = Brushes.Green;
                 btn_Save.Content = "Zaktualizuj";
                 btn_Save.IsEnabled = true;
+                btn_Save.Visibility = Visibility.Visible;
             }
             else
             {
                 lbl_Status.Content = "Nie znaleziono pracownika o nazwisku " + tb_Search.Text;
                 lbl_Status.Foreground = Brushes.Red;
                 btn_Save.IsEnabled = false;
+                ClearTextboxes();
+                btn_Save.Visibility = Visibility.Hidden;
             }
         }
     }
