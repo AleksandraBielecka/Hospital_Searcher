1da2cee2b129e9f668e5e48b2c24b341b8eb91fe
AleksandraBielecka <aleksandrab55@gmail.com>
2018-11-14 21:02:20 +0100

    Adding, deleting employees

--- Hospital_Searcher/MainWindow.xaml.cs
+++ Hospital_Searcher/MainWindow.xaml.cs
@@ -22,22 +22,23 @@ namespace Hospital_Searcher
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int _currentEmployeeId;
+        private int? _currentEmployeeId;
 
         public MainWindow()
         {
             InitializeComponent();
             ClearTextboxes();
             btn_Search.Content = "Szukaj";
-            tb_Search.Text = "";
             lbl_Address.Content = "Address";
             lbl_email.Content = "Email";
             lbl_FirstName.Content = "FirstName";
             lbl_LastName.Content = "LastName";
             lbl_Salary.Content = "Salary";
             lbl_Status.Content = "Użyj wyszukiwarki, żeby znaleźć pracownika";
-            lbl_Telephone.Content = "Content";
+            lbl_Telephone.Content = "Telefon";
             btn_Save.Visibility = Visibility.Hidden;
+            btn_Delete.IsEnabled = false;
+            this.LastNameList();
         }
 
         private void ClearTextboxes()
@@ -52,46 +53,50 @@ namespace Hospital_Searcher
 
         private void btn_Search_Click(object sender, RoutedEventArgs e)
         {
-            bool isLoaded = false;
-            _currentEmployeeId = 0;
-            string CS = @"data source=DESKTOP-E9EAAOK\SQLEXPRESS01;database=HospitalsEmployees;integrated security=SSPI";
-            using (SqlConnection con = new SqlConnection(CS))
+            if (this.rcb_Search.SelectedValue != null)
             {
-                con.Open();
-                SqlCommand cmd = new SqlCommand("Select top 1 ID, FirstName, LastName, Address, Telephone, Email, Salary from tblEmployee where LastName='" + tb_Search.Text + "'", con);
-                using (SqlDataReader rdr = cmd.ExecuteReader())
+                bool isLoaded = false;
+                _currentEmployeeId = 0;
+                string CS = @"data source=DESKTOP-E9EAAOK\SQLEXPRESS01;database=HospitalsEmployees;integrated security=SSPI";
+                using (SqlConnection con = new SqlConnection(CS))
                 {
-                    while (rdr.Read())
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("Select top 1 ID, FirstName, LastName, Address, Telephone, Email, Salary from tblEmployee where LastName='" + this.rcb_Search.SelectedValue.ToString() + "'", con);
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
                     {
-                        var firstName = rdr["FirstName"] as string;
-                        var lastName = rdr["LastName"] as string;
-                        var address = rdr["Address"] as string;
-                        var telephone = rdr["Telephone"] as string;
-                        var email = rdr["Email"] as string;
-                        var salary = Convert.ToInt32(rdr["Salary"]);
-                        tb_FirstName.Text = firstName;
-                        tb_LastName.Text = lastName;
-                        tb_Address.Text = address;
-                        tb_Telephone.Text = telephone;
-                        tb_Email.Text = email;
-                        tb_Salary.Text = salary.ToString();
-                        _currentEmployeeId = Convert.ToInt32(rdr["ID"]);
-                        isLoaded = true;
-                    }
+                        while (rdr.Read())
+                        {
+                            var firstName = rdr["FirstName"] as string;
+                            var lastName = rdr["LastName"] as string;
+                            var address = rdr["Address"] as string;
+                            var telephone = rdr["Telephone"] as string;
+                            var email = rdr["Email"] as string;
+                            var salary = Convert.ToInt32(rdr["Salary"]);
+                            tb_FirstName.Text = firstName;
+                            tb_LastName.Text = lastName;
+                            tb_Address.Text = address;
+                            tb_Telephone.Text = telephone;
+                            tb_Email.Text = email;
+                            tb_Salary.Text = salary.ToString();
+                            _currentEmployeeId = Convert.ToInt32(rdr["ID"]);
+                            isLoaded = true;
+                        }
 
+                    }
                 }
-            }
-            if (isLoaded)
-            {
-                lbl_Status.Content = "Pobrano z bazy danych pracownika";
-                lbl_Status.Foreground = Brushes.Green;
-                btn_Save.Content = "Zaktualizuj";
-                btn_Save.IsEnabled = true;
-                btn_Save.Visibility = Visibility.Visible;
+                if (isLoaded)
+                {
+                    lbl_Status.Content = "Pobrano z bazy danych pracownika";
+                    lbl_Status.Foreground = Brushes.Green;
+                    btn_Save.Content = "Zaktualizuj";
+                    btn_Save.IsEnabled = true;
+                    btn_Save.Visibility = Visibility.Visible;
+                    btn_Delete.IsEnabled = true;
+                }
             }
             else
             {
-                lbl_Status.Content = "Nie znaleziono pracownika o nazwisku " + tb_Search.Text;
+                lbl_Status.Content = "Nie znaleziono pracownika";
                 lbl_Status.Foreground = Brushes.Red;
                 btn_Save.IsEnabled = false;
                 ClearTextboxes();
@@ -105,9 +110,9 @@ namespace Hospital_Searcher
             using (SqlConnection con = new SqlConnection(CS))
             {
                 SqlCommand cmd = new SqlCommand("UPDATE tblEmployee SET FirstName = '" + tb_FirstName.Text + "', LastName = '" + tb_LastName.Text + "', Address = '" + tb_Address.Text + "', Telephone = '" + tb_Telephone.Text + "', Email = '" + tb_Email.Text + "', Salary = " + tb_Salary.Text + " WHERE ID = " + _currentEmployeeId.ToString(), con);
-                con.Open(); 
+                con.Open();
                 int TotalRowsAffected = cmd.ExecuteNonQuery();
-                if (TotalRowsAffected==1)
+                if (TotalRowsAffected == 1)
                 {
                     lbl_Status.Content = "Zaktualizowano dane pracownika";
                     lbl_Status.Foreground = Brushes.Green;
@@ -118,6 +123,80 @@ namespace Hospital_Searcher
                     lbl_Status.Foreground = Brushes.Red;
                 }
 
+            }
+        }
+
+        private void btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            string CS = @"data source=DESKTOP-E9EAAOK\SQLEXPRESS01;database=HospitalsEmployees;integrated security=SSPI";
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("procEmployeeInsert", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@FirstName", tb_FirstName.Text);
+                cmd.Parameters.AddWithValue("@LastName", tb_LastName.Text);
+                cmd.Parameters.AddWithValue("@Address", tb_Address.Text);
+                cmd.Parameters.AddWithValue("@Telephone", tb_Telephone.Text);
+                cmd.Parameters.AddWithValue("@Email", tb_Email.Text);
+                cmd.Parameters.AddWithValue("@Salary", tb_Salary.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                lbl_Status.Content = "Dodano nowego pracownika";
+                lbl_Status.Foreground = Brushes.Green;
+            }
+        }
+
+        private void btn_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            string CS = @"data source=DESKTOP-E9EAAOK\SQLEXPRESS01;database=HospitalsEmployees;integrated security=SSPI";
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("procEmployeeDelete", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@ID", this._currentEmployeeId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                lbl_Status.Content = "Usunięto pracownika";
+                lbl_Status.Foreground = Brushes.Red;
+            }
+
+            tb_FirstName.Text = null;
+            tb_LastName.Text = null;
+            tb_Address.Text = null;
+            tb_Telephone.Text = null;
+            tb_Email.Text = null;
+            tb_Salary.Text = null;
+            _currentEmployeeId = null;
+            btn_Delete.IsEnabled = false;
+        }
+        public void LastNameList()
+        {
+            List<string> Lista = new List<string>();
+            string CS = @"data source=DESKTOP-E9EAAOK\SQLEXPRESS01;database=HospitalsEmployees;integrated security=SSPI";
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("procEmployeeList", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    Lista.Add("Wybierz");
+                    while (rdr.Read())
+                    {
+                        string lastName = rdr["LastName"] as string;
+                        Lista.Add(lastName);
+                    }
+                    
+                    this.rcb_Search.ItemsSource = Lista;
+                    this.rcb_Search.SelectedIndex = 0;
+                }
+                
+                cmd.ExecuteNonQuery();
+                
             }
         }
     }
